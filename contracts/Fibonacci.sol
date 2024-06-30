3e56518b0dcf12ec2a8746c28e8459276838d92b
Stuart Wang <wangshch5@outlook.com>
2024-06-30 14:29:25 +0000

    init evm template

---
+++ contracts/Fibonacci.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Fibonacci {
+    function evaluate(uint256 n) public pure returns (uint256 result) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        uint256 a = 0;
+        uint256 b = 1;
+        uint256 fib;
+
+        for (uint256 i = 2; i <= n; i++) {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+        return fib;
+    }
+}
